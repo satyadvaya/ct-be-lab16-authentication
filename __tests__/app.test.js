fae40b6c84d2449bed8524e83da1c60a2f2ce8f2
satyadvaya <satyadvaya@gmail.com>
2021-10-07 14:14:02 -0700

    failing test POST /signup route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,17 @@ describe('lab16-authentication routes', () => {
     return setup(pool);
   });
 
+  it('signs up a user via POST', async () => {
+    const res = await request(app)
+      .post('/api/auth/signup')
+      .send({ email: 'blowfish@mariner.dingy', password: 'bubbles' });
+
+    expect(res.body).toEqual({
+      id: 1,
+      email: 'blowfish@mariner.dingy',
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
