8d2b7d33adbe26d75d4b2beebec96d9df3a17229
satyadvaya <satyadvaya@gmail.com>
2021-10-07 22:12:50 -0700

    failing test POST /login route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const UserService = require('../lib/services/UserService.js');
 
 describe('lab16-authentication routes', () => {
   beforeEach(() => {
@@ -11,6 +12,22 @@ describe('lab16-authentication routes', () => {
   it('signs up a user via POST', async () => {
     const res = await request(app)
       .post('/api/auth/signup')
+      .send({ email: 'blowfish@mariner.dingy', password: 'bubbles' });
+
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'blowfish@mariner.dingy',
+    });
+  });
+
+  it('logs in a user via POST', async () => {
+    await UserService.create({
+      email: 'blowfish@mariner.dingy',
+      password: 'bubbles',
+    });
+
+    const res = await request(app)
+      .post('/api/auth/login')
       .send({ email: 'blowfish@mariner.dingy', password: 'bubbles' });
 
     expect(res.body).toEqual({
